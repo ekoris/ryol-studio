5fb11e07242fa68fde0baa7c3aa7bdbf31d9f4a6
Eko Rismaryanto <eko.rismaryanto15@gmail.com>
2022-01-06 14:12:45 +0700

    authenticate

--- resources/views/theme/navigation.blade.php
+++ resources/views/theme/navigation.blade.php
@@ -7,41 +7,39 @@
 		{{-- <h1 class="logo me-auto me-lg-0"><a href="">Ryol Studio<span>.</span></a></h1> --}}
 		@if (!Request::routeIs('authentication.product'))
 			<a href="/" class="logo me-auto me-lg-0"><img src="{{ $website->logo_url }}" alt="" class="img-fluid"></a>
-		@else
-			<a href="/" class="logo me-auto me-lg-0"><img src="#" alt="" class="img-fluid"></a>
+			<nav id="navbar" class="navbar order-last order-lg-0">
+				<ul>
+					<li class="dropdown"><a href="#"><span>Art Work</span></a>
+						<ul>
+							@foreach (resolve(App\Repositories\HomeRepository::class)->artWorkYearNavigation() as $item)
+								<li><a href="{{ route('art-work', [$item->year, 'category' => 'painting']) }}">{{ $item->year }}</a></li>
+							@endforeach
+						</ul>
+					</li>
+					<li class="dropdown"><a href="#"><span>Up-Comming Event</span></a>
+						<ul>
+							@foreach (resolve(App\Repositories\Entities\Category::class)->where('type', 2)->get() as $item)
+								<li><a href="{{ route('up-comming', $item->slug) }}">{{ $item->title }}</a></li>
+							@endforeach
+						</ul>
+					</li>
+						<li><a class="nav-link scrollto" href="{{ route('store') }}">Store</a></li>
+					<li class="dropdown"><a href="#"><span>About</span></a>
+						<ul>
+							<li><a href="{{ route('about.cv') }}">Statement</a></li>
+							<li><a href="{{ route('about.cv.periodic') }}">Curriculum Vitae</a></li>
+							<li><a href="{{ route('about.contact-us') }}">Contact Us</a></li>
+						</ul>
+					</li>
+					@if (logged_in_user() && logged_in_user()->hasRole('user'))
+						{{-- <li><a class="nav-link scrollto" href="{{ route('auth.profile') }}">Profile</a></li> --}}
+						<li><a class="nav-link scrollto" href="{{ route('auth.logout') }}">Sign Out</a></li>
+					@else
+						<li><a class="nav-link scrollto" href="{{ route('auth.login') }}">Login</a></li>
+					@endif
+				</ul>
+				<i class="bi bi-list mobile-nav-toggle"></i>
+			</nav>
 		@endif
-		<nav id="navbar" class="navbar order-last order-lg-0">
-			<ul>
-				<li class="dropdown"><a href="#"><span>Art Work</span></a>
-					<ul>
-						@foreach (resolve(App\Repositories\HomeRepository::class)->artWorkYearNavigation() as $item)
-							<li><a href="{{ route('art-work', [$item->year, 'category' => 'painting']) }}">{{ $item->year }}</a></li>
-						@endforeach
-					</ul>
-				</li>
-				<li class="dropdown"><a href="#"><span>Up-Comming Event</span></a>
-					<ul>
-						@foreach (resolve(App\Repositories\Entities\Category::class)->where('type', 2)->get() as $item)
-							<li><a href="{{ route('up-comming', $item->slug) }}">{{ $item->title }}</a></li>
-						@endforeach
-					</ul>
-				</li>
-					<li><a class="nav-link scrollto" href="{{ route('store') }}">Store</a></li>
-				<li class="dropdown"><a href="#"><span>About</span></a>
-					<ul>
-						<li><a href="{{ route('about.cv') }}">Statement</a></li>
-						<li><a href="{{ route('about.cv.periodic') }}">Curriculum Vitae</a></li>
-						<li><a href="{{ route('about.contact-us') }}">Contact Us</a></li>
-					</ul>
-				</li>
-				@if (logged_in_user() && logged_in_user()->hasRole('user'))
-					{{-- <li><a class="nav-link scrollto" href="{{ route('auth.profile') }}">Profile</a></li> --}}
-					<li><a class="nav-link scrollto" href="{{ route('auth.logout') }}">Sign Out</a></li>
-				@else
-					<li><a class="nav-link scrollto" href="{{ route('auth.login') }}">Login</a></li>
-				@endif
-			</ul>
-			<i class="bi bi-list mobile-nav-toggle"></i>
-		</nav>
 	</div>
 </header>
